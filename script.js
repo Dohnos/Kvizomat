1a5840880b9f0f91829c2b1c6c34b2f86c7d0de1
Dohnos <J.dohnos@seznam.cz>
2025-08-01 07:53:24 +0200

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ const db = firebase.database();
 
 let currentQuestion = null;
 let currentUser = { name: null, pin: null, id: null, score: 0, streak: 0, lastAnswerDate: null, lastStreakDate: null };
-const quizStartDate = new Date('2025-07-01T00:00:00');
+const quizStartDate = new Date('2025-08-01T00:00:00');
 let allQuestionsFromDB = [];
 const QUESTION_TIME_LIMIT = 20;
 let questionTimerInterval = null;
