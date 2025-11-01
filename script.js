4d196824f2796451d410224d05abd40c1824e558
Dohnos <J.dohnos@seznam.cz>
2025-11-01 12:35:02 +0100

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ const db = firebase.database();
 
 let currentQuestion = null;
 let currentUser = { name: null, pin: null, id: null, score: 0, streak: 0, lastAnswerDate: null, lastStreakDate: null };
-const quizStartDate = new Date('2025-10-01T00:00:00');
+const quizStartDate = new Date('2025-11-01T00:00:00');
 let allQuestionsFromDB = [];
 const QUESTION_TIME_LIMIT = 25;
 let questionTimerInterval = null;
