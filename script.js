43bd4239445e1133ad46d774ad2d621f56f881d8
Dohnos <J.dohnos@seznam.cz>
2026-07-08 21:55:08 +0200

    Update quiz start date to July 9, 2026

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ const db = firebase.database();
 
 let currentQuestion = null;
 let currentUser = { name: null, pin: null, id: null, score: 0, streak: 0, lastAnswerDate: null, lastStreakDate: null, lastAnswerCorrect: null };
-const quizStartDate = new Date('2026-06-01T00:00:00');
+const quizStartDate = new Date('2026-07-09T00:00:00');
 let allQuestionsFromDB = [];
 const QUESTION_TIME_LIMIT = 30;
 let questionTimerInterval = null;
